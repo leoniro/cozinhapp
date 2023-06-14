a0b87bd0f0b3719978dc3451cb1310faea124286
leoniro <leoniro@gmail.com>
2023-06-14 14:56:14 -0300

    funcionalidade de adicionar receitas implementada

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
+
 var recipes = require('./recipes.json');
-const ingredients = require('./ingredients.json');
+var ingredients = [];
+
+compileIngredients(recipes)
 
 app.use(express.static('./public'));
 app.use(bodyParser.json());
@@ -13,6 +17,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.set("views","./views")
 app.set("view engine","ejs")
 
+// Home: static page
+app.get('/',(req,res) => {
+    res.sendFile('./public/index.html');
+})
+
+// Search: ejs template
 app.post('/busca', (req,res) => {
     let ings = []
     for (let key in req.body) {
@@ -23,8 +33,53 @@ app.post('/busca', (req,res) => {
     res.render('search', {result});
 })
 
-app.get('/',(req,res) => {
-    res.sendFile('./public/index.html');
+// View individual recipe: ejs template
+app.get('/r/:recID', (req, res) => {
+    const {recID} = req.params;
+    const recipe = recipes.find( (r) => r.id === Number(recID) );
+    if (!recipe) {
+        return res.status(404).send('Receita não existe');
+    }
+    res.render("recipe",{recipe});
+})
+
+// Add recipe: static page, then post request
+app.get('/criarReceita', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'public','criarReceita.html'))
+})
+
+app.post('/receitaEnviada', (req,res) => {
+    o = {};
+    o.author = req.body.author;
+    o.name = req.body.name;
+    o.instructions = req.body.instructions;
+    o.ingredients = req.body.ings;
+    if ( Array.isArray(req.body.qtys) ){
+        o.quantities = req.body.qtys;
+    } else {
+        o.quantities = [req.body.qtys];
+    }
+    if ( Array.isArray(req.body.ings) ){
+        o.ingredients = req.body.ings;
+    } else {
+        o.ingredients = [req.body.ings];
+    }
+
+    o.id = recipes.length
+    recipes.push(o)
+    compileIngredients([o])
+    saveRecipesToDisk()
+    res.send('Obrigado por enviar sua receita!')
+})
+
+// API serving ingredients list for form filling
+app.get('/api/ingredients', (req, res) => {
+    res.json(ingredients);
+})
+
+let port = 7777
+app.listen(port,() => {
+    console.log('Server listining on port ' + port);
 })
 
 /*
@@ -36,25 +91,6 @@ app.get('/api', (req, res) => {
     res.json(subReceitas)
 })
 */
-
-app.get('/api/ingredients', (req, res) => {
-    res.json(ingredients);
-    console.log('api/ingredients')
-})
-
-app.get('/api/rec/:recID', (req, res) => {
-    const {recID} = req.params;
-    const recipe = recipes.find( (r) => r.id === Number(recID) );
-    if (!recipe) {
-        return res.status(404).send('Receita não existe');
-    }
-    res.json(recipe);
-})
-
-app.listen(666,() => {
-    console.log('Server listining on port 666');
-})
-
 
 function searchResults(ingreds,recipes) {
     result = [];
@@ -75,7 +111,31 @@ function searchResults(ingreds,recipes) {
         o.quantTotal = quantTotal;
         o.missing = missing;
         result.push(o);
-        
     }
     return result
+}
+
+function compileIngredients(recs) {
+    for (r in recs) {
+        for (i in recs[r].ingredients) {
+            if (ingredients.indexOf(recs[r].ingredients[i]) === -1) {
+                ingredients.push(recs[r].ingredients[i])
+            }
+        }
+    }
+}
+
+function saveRecipesToDisk() {
+    fs.copyFile('./recipes.json', './recipes.json.bak', err => {
+        if (err) {
+            throw err
+        }
+        fs.writeFile('./recipes.json', JSON.stringify(recipes), err => {
+            if (err) {
+                throw err
+            }
+        })
+    })
+    
+    console.log('Nova receita salva')
 }
